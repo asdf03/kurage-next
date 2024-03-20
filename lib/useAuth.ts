956c9303749dfb5ea44122f3d33e0c69638f42a7
masaya nakatsuka <nmsuteado2@yahoo.co.jp>
2024-03-20 10:21:41 +0900

    supabase：ユーザー認証を作成

---
+++ lib/useAuth.ts
@@ -0,0 +1,69 @@
+import supabase from "@/lib/supabase";
+import { Session } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
+
+const useAuth = () => {
+  const [session, setSession] = useState<Session | null>(null);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const { data: authData } = supabase.auth.onAuthStateChange((_, session) => {
+      setSession(session);
+    });
+
+    return () => authData.subscription.unsubscribe();
+  }, []);
+
+  const signInWithEmail = async (email: string, password: string) => {
+    try {
+      const { error } = await supabase.auth.signInWithPassword({ email, password });
+      if (error) {
+        setError(error.message);
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        setError(error.message);
+      } else if (typeof error === "string") {
+        setError(error);
+      } else {
+        console.error("サインインに失敗しました。");
+      }
+    }
+  };
+
+  const signUpWithEmail = async (email: string, password: string) => {
+    try {
+      const { error } = await supabase.auth.signUp({ email, password });
+      if (error) {
+        setError(error.message);
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        setError(error.message);
+      } else if (typeof error === "string") {
+        setError(error);
+      } else {
+        console.error("サインアップに失敗しました。");
+      }
+    }
+  };
+
+  const signOut = async () => {
+    await supabase.auth.signOut();
+  };
+
+  const userProfile = {
+    email: session?.user?.email
+  };
+
+  return {
+    session,
+    error,
+    userProfile,
+    signInWithEmail,
+    signUpWithEmail,
+    signOut,
+  };
+};
+
+export default useAuth;
