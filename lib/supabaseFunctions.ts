38f9fa3585b8fd277e9a82dde0f48a25db162d9f
masaya nakatsuka <nmsuteado2@yahoo.co.jp>
2024-03-20 11:49:22 +0900

    supabase:todoテーブルへのデータ追加機能を作成

---
+++ lib/supabaseFunctions.ts
@@ -0,0 +1,22 @@
+import supabase, { Database } from "./supabase";
+
+export const TABLE_NAME = "todo-app";
+
+export const fetchDatabase =async () => {
+	try {
+    const { data } = await supabase.from(TABLE_NAME).select("*").order("createdAt");
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+type InsertProps = Pick<Database, "title" | "description" | "status" | "due_date">;
+
+export const addSupabaseData = async ({ title, description, status, due_date }: InsertProps) => {
+	try {
+		await supabase.from(TABLE_NAME).insert({ title, description, status, due_date });
+	} catch (error) {
+		console.error(error);
+	}
+};
