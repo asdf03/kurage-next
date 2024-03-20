3a6dcba02a4036c12bc65f686245869d61186b4d
masaya nakatsuka <nmsuteado2@yahoo.co.jp>
2024-03-20 10:48:30 +0900

    supabase:emailのサインインコンポーネント作成

--- app/components/SignInEmail.tsx
+++ app/components/SignInEmail.tsx
@@ -1,17 +1,44 @@
+import React, { useState } from "react";
 import useAuth from "@/lib/useAuth";
 
 const SignInEmail = () => {
-    const { signInWithEmail, error } = useAuth();
+	const { signInWithEmail, error } = useAuth();
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
 
-    return (
-        <div>
-            <button onClick={signInWithEmail}>Emailでサインインする</button>
-            {error && <p>{error}</p>}
-        </div>
-    );
+	const handleSignIn = async (e) => {
+		e.preventDefault();
+		await signInWithEmail(email, password);
+	};
+
+	return (
+		<div>
+			<form onSubmit={handleSignIn}>
+				<div>
+					<label htmlFor="email">メールアドレス：</label>
+					<input
+						id="email"
+						type="email"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+						required
+					/>
+				</div>
+				<div>
+					<label htmlFor="password">パスワード</label>
+					<input
+						id="password"
+						type="password"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+						required
+					/>
+				</div>
+				<button type="submit">Emailでサインインする</button>
+			</form>
+			{error && <p>{error}</p>}
+		</div>
+	);
 };
 
 export default SignInEmail;
-
-
-
