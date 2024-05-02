84642f2b3d0e8ae34a0afb06a32fcad04c5300f7
masaya <nmsuteado2@yahoo.co.jp>
2024-05-02 17:03:02 +0900

    [fetchSupabase]Todoが階層構造になるよう改良

--- lib/fetchSupabaseData.ts
+++ lib/fetchSupabaseData.ts
@@ -2,14 +2,49 @@ import supabase from "./supabase"
 
 const TABLE_NAME = "todo-app"
 
+interface Todo {
+    id: string
+	title: string
+	description: string
+	status: string
+	due_date: string
+	created_at: string
+	parent_id: string
+    children?: Todo[];
+}
+
 export const fetchSupabaseData = async (statusFilter: String | null = null) => {
     try {
         let query = supabase.from(TABLE_NAME).select("*")
         if (statusFilter === 'done') {
             query = query.eq('status', 'done')
         }
-        const { data } = await query
-        return data
+        
+        const { data, error } = await query
+        if (error) {
+            throw error;
+        }
+        if (!data) {
+            return undefined;
+        }
+
+        const todoMap: Record<string, Todo> = {}
+        data.forEach(todo => {
+            todo.children = [];
+            todoMap[todo.id] = todo;    
+        });
+
+        data.forEach(todo => {
+            if (todo.parent_id && todoMap[todo.parent_id]) {
+                todoMap[todo.parent_id].children?.push(todo);
+            }
+        });
+
+        const rootTodos: Todo[] = data.filter(todo => !todo.parent_id);
+
+        console.log(rootTodos)
+
+        return rootTodos
     } catch (error) {
         console.error(error)
     }
