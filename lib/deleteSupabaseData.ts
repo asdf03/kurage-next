e8980b1d92bac88b2f7d7fe02847f04c7268ed25
Masaya Nakatsuka <120952058+asdf03@users.noreply.github.com>
2024-05-18 05:15:13 +0000

    delete機能：子の関係にあるデータも削除されるよう変更

--- lib/deleteSupabaseData.ts
+++ lib/deleteSupabaseData.ts
@@ -2,9 +2,38 @@ import supabase, { Database } from "./supabase"
 
 const TABLE_NAME = "todo-app"
 
+const getAllChildIds = async (parentId: string): Promise<string[]> => {
+    let ids = [parentId]
+    let pendingIds = [parentId]
+
+    while (pendingIds.length > 0) {
+        const currentId = pendingIds.pop()
+        
+        const { data, error } = await supabase
+            .from(TABLE_NAME)
+            .select('id')
+            .eq('parent_id', currentId)
+
+        if (error) {
+            console.error('Error fetching child ids:', error)
+            return []
+        }
+
+        if (data) {
+            const childIds = data.map(item => item.id)
+            ids.push(...childIds)
+            pendingIds.push(...childIds)
+        }
+    }
+
+    return ids
+}
+
 export const deleteSupabaseData = async (id: string) => {
     try {
-        await supabase.from(TABLE_NAME).delete().eq('id', id)
+        const idsToDelete = await getAllChildIds(id);
+        const orCondition = idsToDelete.map(id => `id.eq.${id}`).join(',')
+        await supabase.from(TABLE_NAME).delete().or(orCondition)
     } catch (error) {
 		console.error(error);
 	}
