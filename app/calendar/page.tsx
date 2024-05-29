bfcba125d3718bd37bf122b7ce19414fc83f4546
Masaya Nakatsuka <120952058+asdf03@users.noreply.github.com>
2024-05-29 23:39:21 +0000

    カレンダーを追加

---
+++ app/calendar/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import Calendar from '@/components/calendar/Calendar'
+
+export default function Page() {
+    return (
+        <div className="main">
+            <h2>Calender</h2>
+            <Calendar />
+        </div>
+    )
+}
+
---
+++ components/calendar/Calendar.tsx
@@ -0,0 +1,47 @@
+"use client"
+
+import React from 'react';
+import { useState, useEffect } from 'react';
+import styles from './Calendar.module.css';
+
+const Calendar = () => {
+    const [currentDate, setCurrentDate] = useState(new Date())
+    const [localeDate, setLocaleDate] = useState<string | null>(null)
+
+    useEffect(() => {
+        setLocaleDate(currentDate.toLocaleString('default', { month: 'long' }));
+    }, [currentDate]);
+
+    const startOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1)
+    const endOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0)
+
+    const daysInMonth: number[] = []
+    for (let i = 1; i <= endOfMonth.getDate(); i++) {
+        daysInMonth.push(i)
+    }
+
+    const prevMonth = () => {
+        setCurrentDate(new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 1))
+    }
+
+    const nextMonth = () => {
+        setCurrentDate(new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 1));
+    };
+
+    return (
+        <div className={styles.calendar}>
+            <div className={styles.header}>
+                <button onClick={prevMonth}>&lt;</button>
+                <h2>{localeDate}{currentDate.getFullYear()}</h2>
+                <button onClick={nextMonth}>&gt;</button>
+            </div>
+            <div className={styles.days}>
+                {daysInMonth.map(day => (
+                    <div key={day} className={styles.day}>{day}</div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default Calendar
